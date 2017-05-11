002dcf654658847bae807dc3fb294561f068eb37
chongieball <chongg1243@gmail.com>
2017-05-11 23:17:28 +0700

    change

--- app/middlewares.php
+++ app/middlewares.php
@@ -10,9 +10,4 @@
 
 ######WEB###############
 
-//When response status 404, auto call not found page
-$app->add(new \MBS\Middlewares\NotFoundMiddleware($container));
 
-//csrf
-$app->add(new \MBS\Middlewares\CsrfMiddleware($container));
-$app->add($container->csrf);
