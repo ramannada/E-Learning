70793cc37302f152113f317973d7865e87982dd8
chongieball <chongg1243@gmail.com>
2017-05-12 01:39:50 +0700

    add middleware

--- app/routes.php
+++ app/routes.php
@@ -4,7 +4,7 @@ $app->group('/api', function() use ($app,$container) {
 	$app->post('/register', 'App\Controllers\Api\UserController:register');
 	$app->get('/active', 'App\Controllers\Api\UserController:activeUser')->setName('api.user.active');
 	$app->post('/login', 'App\Controllers\Api\UserController:login');
-});
+})->add(new \App\Middlewares\Api\AuthToken($container));
 
 $app->group('', function() use ($app,$container) {
 	$app->get('/register', 'App\Controllers\Web\UserController:getRegister')->setName('web.user.register');
