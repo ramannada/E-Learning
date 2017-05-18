de258828d40a6e734d273c7ac0553a14a07bec9b
chongieball <chongg1243@gmail.com>
2017-05-18 15:55:35 +0700

    name route

--- app/apiRoute.php
+++ app/apiRoute.php
@@ -1,11 +1,11 @@
 <?php
 
 $app->group('/api', function() use ($app,$container) {
-	$app->post('/register', 'App\Controllers\Api\UserController:register');
+	$app->post('/register', 'App\Controllers\Api\UserController:register')->setName('api.user.register');
 
 	$app->get('/active', 'App\Controllers\Api\UserController:activeUser')->setName('api.user.active');
 
-	$app->post('/login', 'App\Controllers\Api\UserController:login');
+	$app->post('/login', 'App\Controllers\Api\UserController:login')->setName('api.user.login');
 
     $app->post('/password_reset', 'App\Controllers\Api\UserController:passwordReset')->setName('api.user.password.reset');
     $app->get('/renew_password', 'App\Controllers\Api\UserController:getReNewPassword')->setName('api.user.get.renew.password');
